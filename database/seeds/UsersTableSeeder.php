49bf87c2140e8e960a72d8fab9ac8379c5a42390
Duc Bui <ducbui1211@gmail.com>
2023-02-04 23:08:21 +0700

    Push DACN

---
+++ database/seeds/UsersTableSeeder.php
@@ -0,0 +1,56 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+
+class UsersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        
+        DB::table('users')->insert([
+            'name' => 'System Admin',
+            'email' => 'admin@dev.com',
+            'password' => bcrypt('admin123'),
+            'created_at' =>  date("Y-m-d H:i:s")
+        ]);
+
+        $user = \App\User::where('email','admin@dev.com')->first();
+        $user->assignRole('administrator');
+
+        DB::table('users')->insert([
+            'name' => 'Vendor 01',
+            'email' => 'vendor1@dev.com',
+            'password' => bcrypt('admin123'),
+            'created_at' =>  date("Y-m-d H:i:s")
+        ]);
+        $user = \App\User::where('email','vendor1@dev.com')->first();
+        $user->assignRole('vendor');
+
+        DB::table('users')->insert([
+            'name' => 'Customer 01',
+            'email' => 'customer1@dev.com',
+            'password' => bcrypt('admin123'),
+            'created_at' =>  date("Y-m-d H:i:s")
+        ]);
+
+        $user = \App\User::where('email','customer1@dev.com')->first();
+        $user->assignRole('customer');
+
+
+        DB::table('users')->insert([
+            'name' => 'Duc Bui',
+            'email' => 'duc@gmail.com',
+            'password' => bcrypt('admin123'),
+            'created_at' =>  date("Y-m-d H:i:s")
+        ]);
+        $user = \App\User::where('email','duc@gmail.com')->first();
+        $user->assignRole('administrator');
+    }
+}
